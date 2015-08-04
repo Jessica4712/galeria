2906877140237d5b48124726ef5a85e769e2c2c1
jessica4712 <jessy92_04@hotmail.com>
2015-08-04 17:25:18 -0500

    nathalia

---
+++ galeria.js
@@ -0,0 +1,18 @@
+function hola2() {
+	document.getElementById('imagenprincipal').src="./imagenes/imagen2.jpg";
+	
+}
+function hola3() {
+	document.getElementById('imagenprincipal').src="./imagenes/imagen3.jpg";
+}
+function hola4() {
+	document.getElementById('imagenprincipal').src="./imagenes/imagen4.jpg";
+}
+function hola5() {
+	document.getElementById('imagenprincipal').src="./imagenes/imagen5.jpg";
+}
+
+function volver() {
+	document.getElementById('imagenprincipal').src="./imagenes/imagenprincipal.jpg";
+}
+
